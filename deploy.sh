12f084d7d31e874b96222912ef94110c3b73f52e
Michael Bolland <233287+mixxit@users.noreply.github.com>
2019-09-19 14:11:17 +0100

    hugo url

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ if [ -n "$GITHUB_AUTH_SECRET" ]
 then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
-    echo $GITHUB_AUTH_SECRET > ~/.git-credentials    git config credential.helper store
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials    
+    git config credential.helper store
     git config user.email "mixxit-bot@users.noreply.github.com"
     git config user.name "mixxit-bot"
 fi
